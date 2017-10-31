6e08ed44e7c6db7d7d607468621d53fc77c06b9c
Maciej Bednarczuk <maciejbednarczuk@gmail.com>
2017-10-31 16:00:04 +0100

    Models

--- movielist/models.py
+++ movielist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    director = models.ForeignKey(Person, related_name="movies_directed")
+    year = models.SmallIntegerField()
+    actors = models.ManyToManyField(Person, related_name="movies_cast")
