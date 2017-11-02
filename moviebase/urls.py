ca91000bae68d9d37c2a6cf8e15912ca6e55901b
Maciej Bednarczuk <maciejbednarczuk@gmail.com>
2017-11-02 16:11:57 +0100

    Next improvments

--- moviebase/urls.py
+++ moviebase/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf.urls import url
+from django.contrib import admin
+
+from movielist.views import MovieListView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^movies$', MovieListView.as_view())
 ]
--- movielist/views.py
+++ movielist/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from movielist.models import Movie
+from movielist.serializers import MovieSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class MovieListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
